da90ecd787fef9bfb6d12cf87acbba9390948654
gauravsetia21 <gaurav_setia@yahoo.com>
2016-04-30 04:20:56 -0700

    Create projecteuler6.py

---
+++ projecteuler6.py
@@ -0,0 +1,11 @@
+t=eval(raw_input())
+d=[i**2 for i in range(10001)]
+for k in range(t):
+	n=eval(raw_input())
+	sums=(n*(n+1))/2
+	sums1=sums**2
+	r=n+1
+	sqsum=0
+	for p in range(n+1):
+		sqsum+=d[p]
+	print (sums1)-sqsum
