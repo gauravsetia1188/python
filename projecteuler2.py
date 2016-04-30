cf1ac48442581085d072a7d4ecf32f104cc7998a
gauravsetia21 <gaurav_setia@yahoo.com>
2016-04-30 04:17:35 -0700

    Create projecteuler2.py

---
+++ projecteuler2.py
@@ -0,0 +1,16 @@
+t=eval(raw_input())
+for k in range(t):
+	n=eval(raw_input())
+	x=1
+	y=2
+	evensum=2
+	next=0
+	while 1:
+		next=x+y
+		if next>n:
+			break
+		if next%2==0:
+			evensum=evensum+next
+		x=y
+		y=next
+	print evensum
