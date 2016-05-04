e159de4a7a923283d35c3f3e69ab8031e4f821ca
gauravsetia21 <gaurav_setia@yahoo.com>
2016-05-04 03:27:23 -0700

    Update projecteuler6.py

--- projecteuler6.py
+++ projecteuler6.py
@@ -1,9 +1,10 @@
+#Find the difference between the sum of the squares of the first N natural numbers and the square of the sum.
 t=eval(raw_input())
-d=[i**2 for i in range(10001)]
+d=[i**2 for i in range(10001)]   #initialising the list
 for k in range(t):
 	n=eval(raw_input())
 	sums=(n*(n+1))/2
-	sums1=sums**2
+	sums1=sums**2 
 	r=n+1
 	sqsum=0
 	for p in range(n+1):
