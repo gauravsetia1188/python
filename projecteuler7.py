ea0dcf8e8a04b773842bd31ba8521a260253bd80
gauravsetia21 <gaurav_setia@yahoo.com>
2016-05-04 03:31:56 -0700

    Update projecteuler7.py

--- projecteuler7.py
+++ projecteuler7.py
@@ -1,22 +1,24 @@
+# find nth prime no
+#algorithm used sieve of eratothenes,prcomputation
 from math import *
 x=10005
 j=0
-prime=[1 for j in range(100000)]
+prime=[1 for j in range(100000)]   #setting all no's in the list as true, precomputation
 p=2
-while p < int(floor(sqrt(x))):
+while p < int(floor(sqrt(x))):   #sieve of erastothenes
 	if prime[p]==1:
 		i=p*2
 		while i<=x:
 			prime[i]=0
 			i+=p
-	p+=1
+	p+=1       
 p=2
 s=[]
-while p<=x:
+while p<=x:      #storing all prime no's computed above in a separate list
 	if prime[p]==1:
 		s.append(p)
 	p+=1
-t=eval(raw_input())
+t=eval(raw_input())   #no of test cases
 for k in range(t):
 	n=eval(raw_input())
-	print s[n-1]
+	print s[n-1]  #directly picking from the precomputed list in O(1) time complexity
