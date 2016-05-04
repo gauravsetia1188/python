ee53a15cc4b240087269271265fa93f3b27c3e4b
gauravsetia21 <gaurav_setia@yahoo.com>
2016-05-04 03:25:25 -0700

    Update projecteuler5.py

--- projecteuler5.py
+++ projecteuler5.py
@@ -1,6 +1,8 @@
+#What is the smallest positive number that is evenly divisible(divisible with no remainder) by all of the numbers from 1 to N?
+#e.g 2520  is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder. 
 def gcd(a,b):
 	if a%b==0:
-		return b;
+		return b;   #gcd using recursion
 	if b%a==0:
 		return a;
 	if a>b:
@@ -8,7 +10,7 @@ def gcd(a,b):
 	if b>a:
 		return gcd(a,b%a);
  
-t=eval(raw_input())
+t=eval(raw_input())   #logic:we need to find lowest common multiple of all the numbers uptil n
 for k in range(t):
 	n=eval(raw_input())
 	ans=1
@@ -17,9 +19,9 @@ for k in range(t):
 		if i==0:
 			pass
 		a=c*(i+1)
-		b=gcd(c,i+1)
+		b=gcd(c,i+1)    #formula for lcm=a*b/gcd(a,b)
 		c=a/b
-	if n==1:
+	if n==1:    #corner case
 		print '1'
 	else:
 		print c
