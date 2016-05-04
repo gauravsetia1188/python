1dcc3d60b93871126c0646b8859c61a66cd8de1d
gauravsetia21 <gaurav_setia@yahoo.com>
2016-05-04 03:18:57 -0700

    Update projecteuler3.py

--- projecteuler3.py
+++ projecteuler3.py
@@ -1,3 +1,5 @@
+#largest prime factor of a given no n
+#2 conditions are needed to be checked :1-no is prime 2-it is a factor of n
 from math import *
 t=eval(raw_input())
  
@@ -7,8 +9,8 @@ def isprime(i):
 	elif i==1 or i%2==0:
 		return 0
 	else:
-		p=3
-		while p<ceil(sqrt(i)):
+		p=3			
+		while p<ceil(sqrt(i)):   #prime checking
 			if i%p==0:
 				return 0
 			p+=2
@@ -17,12 +19,12 @@ def isprime(i):
  
 for k in range(t):
 	n=eval(raw_input())
-	i=3
+	i=3   #started from 3 an is incremented by 2 so as to avoid even no prime checking as 2 is the only prime no
 	maxi=0
-	while i<=n:
+	while i<=n:    # for all i less than n it checks whether i is a factor of n and if it is then it checks whether it is prime
 		if n%i==0:
 			x=isprime(i)
 			if x==1:
 				maxi=i
 		i+=2
-	print maxi
+	print maxi   #maxi is largest prime factor
