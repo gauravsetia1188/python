27d0a42763622f9d5b89da62863628a774ef30ae
gauravsetia21 <gaurav_setia@yahoo.com>
2016-05-04 03:35:56 -0700

    Update projecteuler8.py

--- projecteuler8.py
+++ projecteuler8.py
@@ -1,3 +1,4 @@
+#Find the greatest product of K consecutive digits in the N digit number.
 t=eval(raw_input())
 for k in range(t):
 	n,k=map(int,raw_input().split(' '))
@@ -5,14 +6,14 @@ for k in range(t):
 	s=list(d)
 	product=1
 	maxproduct=0
-	for i in range(n-k+1):
-		j=i
+	for i in range(n-k+1):   #Brute force method used
+		j=i		  #picks first no from the list and finds product of k no's then i is incremented and so on
 		product=1
 		r=k
 		while r>0:
-			product=product*int(s[j])
-			r-=1
+			product=product*int(s[j])  #product variable stores the present product
+			r-=1			   #int is used to explicitly type cast the string value
 			j+=1
-		if product>maxproduct:
+		if product>maxproduct:  #max product stores the largest product uptil now
 			maxproduct=product
 	print maxproduct
